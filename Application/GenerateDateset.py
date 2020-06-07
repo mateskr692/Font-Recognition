ceb6cb7624b1e0a7010654c9702f17caac265f0f
Mateusz Skrzelowski (mateskr692) <mateskr692@student.polsl.pl>
2020-06-08 00:01:21 +0200

    CNN structure

--- Application/GenerateDateset.py
+++ Application/GenerateDateset.py
@@ -1,59 +1,104 @@
 import trdg.generators as gens
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import random
 from PIL import Image
 import os
 
 
-def GenerareDataset() -> None:
+class data_generator:
 
-    fontsDir = r".venv\Lib\site-packages\trdg\fonts\latin"
-    outDir = r"Dataset"
-    fontsDictionary = open(outDir + r"\fonts.txt", "w")
+    def GenerareDataset(self, fontsDir: str = r".venv\Lib\site-packages\trdg\fonts\latin",  outDir: str = r"Dataset") -> None:
 
-    for filename in os.listdir(fontsDir):
+        os.mkdir(os.path.join(outDir, "Train"))
+        os.mkdir(os.path.join(outDir, "Test"))
+        for filename in os.listdir(fontsDir):
 
-        # create directory and add fontname to dictionary
-        fontName = os.path.splitext(filename)[0]
-        fontFullPath = fontsDir + "\\" + filename
-        fontImageDirectory = outDir + "\\" + fontName
-        os.mkdir(fontImageDirectory)
-        fontsDictionary.write(fontName + "\n")
+            fontName = os.path.splitext(filename)[0]
+            fontFullPath = os.path.join(fontsDir, filename)
 
-        imgIndex = 0
+            fontTrainDir = os.path.join(outDir, "Train", fontName)
+            fontTestDir = os.path.join(outDir, "Test", fontName)
+            os.mkdir(fontTestDir)
+            os.mkdir(fontTrainDir)
 
-        def generate(count: int, generator: gens.GeneratorFromWikipedia) -> None:
-            i = 0
-            for img, _ in generator:
-                img.save(fontImageDirectory + "\\" + str(imgIndex) + ".jpg")
-                i += 1
-                imgIndex += 1
-                if i > count:
+            imgIndex = 0
+
+            def generate(path: str, generator: gens.GeneratorFromDict) -> None:
+                nonlocal imgIndex
+                for [img, _] in generator:
+                    img.save(os.path.join(path, str(imgIndex) + ".jpg"))
+                    imgIndex += 1
                     break
+                return None
 
-            return None
+            # generate data :
+            generator = gens.GeneratorFromDict(
+                fonts=[fontFullPath],
+                random_blur=True,
+                random_skew=True,
+                blur=1,
+                skewing_angle=3,
+                image_dir="Backgrounds",
+            )
 
-        # generate data
-        generator = gens.GeneratorFromWikipedia(
-            fonts=[fontFullPath],
-            random_blur=True,
-            random_skew=True,
-            blur=1,
-            skewing_angle=3,
-        )
-        generate(20, generator)
+            # Train Data
+            # colored text on noisy background
+            for _ in range(0, 200):
+                color = '#' + \
+                    str(format(random.randint(20, 160), 'x')) + \
+                    str(format(random.randint(20, 160), 'x')) + \
+                    str(format(random.randint(20, 160), 'x'))
 
-        break
+                generator.generator.text_color = color
+                generator.generator.background_type = random.randint(0, 2)
+                generate(fontTrainDir, generator)
 
-    return None
+            # colored text on background
+            generator.generator.random_blur = False
+            generator.generator.blur = 0
+            generator.generator.background_type = 3
+            for _ in range(0, 50):
+                color = '#' + \
+                    str(format(random.randint(20, 80), 'x')) + \
+                    str(format(random.randint(20, 80), 'x')) + \
+                    str(format(random.randint(20, 80), 'x'))
 
+                generator.generator.text_color = color
+                generate(fontTrainDir, generator)
 
-def GenerateRandomImage(text: str = "") -> Image:
-    if(text == ""):
-        generator = gens.GeneratorFromDict()
-        for img, _ in generator:
-            return img
-    else:
-        generator = gens.GeneratorFromWikipedia()
-        for img, _ in generator:
-            return img
+            # Test Data
+            for _ in range(0, 20):
+                color = '#' + \
+                    str(format(random.randint(20, 160), 'x')) + \
+                    str(format(random.randint(20, 160), 'x')) + \
+                    str(format(random.randint(20, 160), 'x'))
+
+                generator.generator.text_color = color
+                generator.generator.background_type = random.randint(0, 2)
+                generate(fontTestDir, generator)
+
+            # colored text on background
+            generator.generator.random_blur = False
+            generator.generator.blur = 0
+            generator.generator.background_type = 3
+            for _ in range(0, 5):
+                color = '#' + \
+                    str(format(random.randint(20, 80), 'x')) + \
+                    str(format(random.randint(20, 80), 'x')) + \
+                    str(format(random.randint(20, 80), 'x'))
+
+                generator.generator.text_color = color
+                generate(fontTestDir, generator)
+
+        return None
+
+    def GenerateRandomImage(self, text: str = "") -> Image:
+        if(text == ""):
+            generator = gens.GeneratorFromDict()
+            for img, _ in generator:
+                return img
+        else:
+            generator = gens.GeneratorFromWikipedia()
+            for img, _ in generator:
+                return img
--- Application/model.py
+++ Application/model.py
@@ -1,65 +1,112 @@
 import tensorflow as tf
 import typing
 from tensorflow import keras
-
-# prepares a proper model layer structure
-
-
-def prepare_model() -> keras.Sequential:
-    model = keras.Sequential(
-        [
-            keras.layers.Flatten(input_shape=(28, 28)),
-            keras.layers.Dense(128, activation='relu'),
-            keras.layers.Dense(10)
-        ])
-
-    model.compile(
-        optimizer='adam',
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=['accuracy']
-    )
-
-    return model
-
-# trains a model on given data
+from keras import layers
+import numpy as np
+import os
+import pathlib
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
-def train_model(model: keras.Sequential) -> None:
+# model.load_dataset()
+# model.build()
+# model.train()
+# model.save()
+# model.load()
+# model.predict()
+class model:
 
-    fashion_mnist = keras.datasets.fashion_mnist
-    (train_images, train_labels), (test_images,
-                                   test_labels) = fashion_mnist.load_data()
+    IMAGE_WIDTH: int = 120
+    IMAGE_HEIGHT: int = 120
+    BATCH_SIZE: int = 50
 
-    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    CLASS_COUNT: int = 0
+    CLASS_NAMES: typing.List[str] = []
+    EPOCHS: int = 10
 
-    train_images = train_images / 255.0
-    test_images = test_images / 255.0
-    model.fit(train_images,
-              train_labels,
-              epochs=10)
+    model: keras.Sequential
+    train_ds: tf.data.Dataset
+    test_ds: tf.data.Dataset
 
-    return None
+    def load_dataset(self, data_path: str = r"Dataset") -> None:
+
+        self.CLASS_NAMES = os.listdir(os.path.join(data_path, "Test"))
+        self.CLASS_COUNT = len(self.CLASS_NAMES)
+
+        print("class count: " + str(self.CLASS_COUNT))
+        print(self.CLASS_NAMES)
+
+        self.train_ds = image_dataset_from_directory(
+            directory=os.path.join(data_path, "Train"),
+            labels='inferred',
+            label_mode='categorical',
+            batch_size=32,
+            image_size=(120, 120)
+        )
+
+        self.test_ds = image_dataset_from_directory(
+            directory=os.path.join(data_path, "Test"),
+            labels='inferred',
+            label_mode='categorical',
+            batch_size=32,
+            image_size=(120, 120)
+        )
+
+    def build(self) -> None:
+
+        self.model = keras.Sequential()
+        # Cu Layers
+        self.model.add(layers.Conv2D(64, kernel_size=(
+            48, 48), activation='relu', input_shape=(self.IMAGE_WIDTH, self.IMAGE_HEIGHT, 3)))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(layers.Conv2D(
+            128, kernel_size=(24, 24), activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(layers.Conv2DTranspose(128, (24, 24), strides=(
+            2, 2), activation='relu', padding='same', kernel_initializer='uniform'))
+        self.model.add(layers.UpSampling2D(size=(2, 2)))
+        self.model.add(layers.Conv2DTranspose(64, (12, 12), strides=(
+            2, 2), activation='relu', padding='same', kernel_initializer='uniform'))
+        self.model.add(layers.UpSampling2D(size=(2, 2)))
+
+        # Cs Layers
+        self.model.add(layers.Conv2D(
+            256, kernel_size=(12, 12), activation='relu'))
+        self.model.add(layers.Conv2D(
+            256, kernel_size=(12, 12), activation='relu'))
+        self.model.add(layers.Conv2D(
+            256, kernel_size=(12, 12), activation='relu'))
+
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(4096, activation='relu'))
+        self.model.add(layers.Dropout(0.5))
+        self.model.add(layers.Dense(4096, activation='relu'))
+        self.model.add(layers.Dropout(0.5))
+        self.model.add(layers.Dense(2383, activation='relu'))
+        self.model.add(layers.Dense(self.CLASS_COUNT, activation='softmax'))
+
+        sgd = keras.optimizers.SGD(
+            lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(loss='mean_squared_error',
+                           optimizer=sgd, metrics=['accuracy'])
+
+        return None
+
+    # trains a model on given data
+
+    def train(self) -> None:
+
+        self.model.fit(self.train_ds, self.EPOCHS,
+                       validation_data=self.test_ds)
+
+        return None
 
 
-# # Create a callback that saves the model's weights
-# cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-#                                                  save_weights_only=True,
-#                                                  verbose=1)
-
-# # Train the model with the new callback
-# model.fit(train_images,
-#           train_labels,
-#           epochs=10,
-#           validation_data=(test_images,test_labels),
-#           callbacks=[cp_callback])  # Pass callback to training
-
-
-# # Save the weights
-# model.save_weights('./checkpoints/my_checkpoint')
-# # Restore the weights
-# model.load_weights('./checkpoints/my_checkpoint')
-
-
-# model.save('saved_model/my_model')
-# new_model = tf.keras.models.load_model('saved_model/my_model')
+m = model()
+m.load_dataset()
+m.build()
+# m.train()
