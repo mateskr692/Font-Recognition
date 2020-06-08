92ae2b4a040adf13338d75b82c09860b21b67812
Mateusz Skrzelowski (mateskr692) <mateskr692@student.polsl.pl>
2020-06-08 16:52:01 +0200

    sample fonts, improved generation

--- Application/GenerateDateset.py
+++ Application/GenerateDateset.py
@@ -8,31 +8,28 @@ import os
 
 class data_generator:
 
-    def GenerareDataset(self, fontsDir: str = r".venv\Lib\site-packages\trdg\fonts\latin",  outDir: str = r"Dataset") -> None:
+    fontsDir: str
+    outDir: str
 
-        os.mkdir(os.path.join(outDir, "Train"))
-        os.mkdir(os.path.join(outDir, "Test"))
-        for filename in os.listdir(fontsDir):
+    def __init__(self, fontsDir: str = r"Fonts",  outDir: str = r"Dataset"):
+        self.fontsDir = fontsDir
+        self.outDir = outDir
+
+    def GenerareDataset(self) -> None:
+
+        os.mkdir(os.path.join(self.outDir, "Train"))
+        os.mkdir(os.path.join(self.outDir, "Test"))
+        for filename in os.listdir(self.fontsDir):
 
             fontName = os.path.splitext(filename)[0]
-            fontFullPath = os.path.join(fontsDir, filename)
+            fontFullPath = os.path.join(self.fontsDir, filename)
 
-            fontTrainDir = os.path.join(outDir, "Train", fontName)
-            fontTestDir = os.path.join(outDir, "Test", fontName)
+            fontTrainDir = os.path.join(self.outDir, "Train", fontName)
+            fontTestDir = os.path.join(self.outDir, "Test", fontName)
             os.mkdir(fontTestDir)
             os.mkdir(fontTrainDir)
 
             imgIndex = 0
-
-            def generate(path: str, generator: gens.GeneratorFromDict) -> None:
-                nonlocal imgIndex
-                for [img, _] in generator:
-                    img.save(os.path.join(path, str(imgIndex) + ".jpg"))
-                    imgIndex += 1
-                    break
-                return None
-
-            # generate data :
             generator = gens.GeneratorFromDict(
                 fonts=[fontFullPath],
                 random_blur=True,
@@ -40,65 +37,96 @@ class data_generator:
                 blur=1,
                 skewing_angle=3,
                 image_dir="Backgrounds",
+                size=64,
             )
 
-            # Train Data
-            # colored text on noisy background
             for _ in range(0, 200):
-                color = '#' + \
-                    str(format(random.randint(20, 160), 'x')) + \
-                    str(format(random.randint(20, 160), 'x')) + \
-                    str(format(random.randint(20, 160), 'x'))
+                size = random.randint(32, 80)
+                bg_type = random.randint(0, 3)
+                brightness = '{:02x}'.format(random.randint(0, 50))
 
-                generator.generator.text_color = color
-                generator.generator.background_type = random.randint(0, 2)
-                generate(fontTrainDir, generator)
+                generator.generator.size = size
+                generator.generator.background_type = bg_type
+                generator.generator.text_color = "#" + brightness*3
+                generator.generator.margins = (random.randint(2, 10),
+                                               random.randint(2, 10),
+                                               random.randint(2, 10),
+                                               random.randint(2, 10))
 
-            # colored text on background
-            generator.generator.random_blur = False
-            generator.generator.blur = 0
-            generator.generator.background_type = 3
-            for _ in range(0, 50):
-                color = '#' + \
-                    str(format(random.randint(20, 80), 'x')) + \
-                    str(format(random.randint(20, 80), 'x')) + \
-                    str(format(random.randint(20, 80), 'x'))
+                def generate(path: str, amount: int) -> None:
+                    nonlocal imgIndex
+                    nonlocal generator
+                    nonlocal size
+                    nonlocal bg_type
+                    indx = 0
+                    for [img, _] in generator:
+                        img = img.convert('L')
+                        img.save(os.path.join(
+                            path, str(imgIndex) + "_size" + str(size) +
+                            "_bgtype" + str(bg_type) + ".jpg"))
+                        imgIndex += 1
+                        indx += 1
+                        if indx >= amount:
+                            break
+                    return None
 
-                generator.generator.text_color = color
-                generate(fontTrainDir, generator)
-
-            # Test Data
-            for _ in range(0, 20):
-                color = '#' + \
-                    str(format(random.randint(20, 160), 'x')) + \
-                    str(format(random.randint(20, 160), 'x')) + \
-                    str(format(random.randint(20, 160), 'x'))
-
-                generator.generator.text_color = color
-                generator.generator.background_type = random.randint(0, 2)
-                generate(fontTestDir, generator)
-
-            # colored text on background
-            generator.generator.random_blur = False
-            generator.generator.blur = 0
-            generator.generator.background_type = 3
-            for _ in range(0, 5):
-                color = '#' + \
-                    str(format(random.randint(20, 80), 'x')) + \
-                    str(format(random.randint(20, 80), 'x')) + \
-                    str(format(random.randint(20, 80), 'x'))
-
-                generator.generator.text_color = color
-                generate(fontTestDir, generator)
+                generate(fontTestDir, 1)
+                generate(fontTrainDir, 10)
 
         return None
 
-    def GenerateRandomImage(self, text: str = "") -> Image:
-        if(text == ""):
-            generator = gens.GeneratorFromDict()
-            for img, _ in generator:
-                return img
-        else:
-            generator = gens.GeneratorFromWikipedia()
-            for img, _ in generator:
-                return img
+    def GenerateRandomImage(self, text: str = "", font: str = "") -> Image:
+
+        filename: str
+        fontname: str
+
+        generator = gens.GeneratorFromDict(
+            random_blur=True,
+            random_skew=True,
+            blur=1,
+            skewing_angle=3,
+            image_dir="Backgrounds",
+            size=64,
+        )
+
+        if(text != ""):
+            generator = gens.GeneratorFromStrings(
+                strings=[text],
+                random_blur=True,
+                random_skew=True,
+                blur=1,
+                skewing_angle=3,
+                image_dir="Backgrounds",
+                size=64,
+            )
+
+        if(font != ""):
+            if(os.path.isfile(font)):
+                filename = font
+                fontname = os.path.splitext(filename)[0]
+                generator.fonts = [filename]
+
+            else:
+                print("Invalid font, picking one at random")
+
+        filename = random.choice(os.listdir(self.fontsDir))
+        fontname = os.path.splitext(filename)[0]
+        generator.fonts = [os.path.join(self.fontsDir, filename)]
+
+
+# g = gens.GeneratorFromDict(
+#     random_blur=False,
+#     random_skew=False,
+#     blur=0,
+#     skewing_angle=0,
+#     image_dir="Backgrounds",
+#     size=64,
+#     fit=True,
+#     margins=(random.randint(0, 10), random.randint(0, 10),
+#              random.randint(0, 10), random.randint(0, 10))
+# )
+# for i, _ in g:
+#     i.save("Test.jpg")
+#     break
+gen = data_generator()
+gen.GenerareDataset()
--- Application/model.py
+++ Application/model.py
@@ -1,9 +1,7 @@
+import os
 import tensorflow as tf
 import typing
-from tensorflow import keras
-from keras import layers
 import numpy as np
-import os
 import pathlib
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image_dataset_from_directory
@@ -17,15 +15,15 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 # model.predict()
 class model:
 
-    IMAGE_WIDTH: int = 120
-    IMAGE_HEIGHT: int = 120
-    BATCH_SIZE: int = 50
+    IMAGE_WIDTH: int = 105
+    IMAGE_HEIGHT: int = 105
+    BATCH_SIZE: int = 128
 
     CLASS_COUNT: int = 0
     CLASS_NAMES: typing.List[str] = []
-    EPOCHS: int = 10
+    EPOCHS: int = 20
 
-    model: keras.Sequential
+    model: tf.keras.Sequential
     train_ds: tf.data.Dataset
     test_ds: tf.data.Dataset
 
@@ -37,59 +35,73 @@ class model:
         print("class count: " + str(self.CLASS_COUNT))
         print(self.CLASS_NAMES)
 
+        f = open("NetworkInfo.txt", "w")
+        f.write("Output layer node count: " + str(self.CLASS_COUNT) + "\n\n")
+        f.writelines(self.CLASS_NAMES)
+        f.close()
+
         self.train_ds = image_dataset_from_directory(
             directory=os.path.join(data_path, "Train"),
             labels='inferred',
             label_mode='categorical',
-            batch_size=32,
-            image_size=(120, 120)
+            batch_size=self.BATCH_SIZE,
+            image_size=(self.IMAGE_WIDTH, self.IMAGE_HEIGHT),
+            color_mode="grayscale"
         )
 
         self.test_ds = image_dataset_from_directory(
             directory=os.path.join(data_path, "Test"),
             labels='inferred',
             label_mode='categorical',
-            batch_size=32,
-            image_size=(120, 120)
+            batch_size=self.BATCH_SIZE,
+            image_size=(self.IMAGE_WIDTH, self.IMAGE_HEIGHT),
+            color_mode="grayscale"
         )
+
+        self.train_ds.batch(self.BATCH_SIZE)
+        self.test_ds.batch(self.BATCH_SIZE)
 
     def build(self) -> None:
 
-        self.model = keras.Sequential()
-        # Cu Layers
-        self.model.add(layers.Conv2D(64, kernel_size=(
-            48, 48), activation='relu', input_shape=(self.IMAGE_WIDTH, self.IMAGE_HEIGHT, 3)))
-        self.model.add(layers.BatchNormalization())
-        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(layers.Conv2D(
-            128, kernel_size=(24, 24), activation='relu'))
-        self.model.add(layers.BatchNormalization())
-        self.model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+        self.model = tf.keras.Sequential()
 
-        self.model.add(layers.Conv2DTranspose(128, (24, 24), strides=(
+        # Cu Layers
+        self.model.add(tf.keras.layers.Conv2D(64, kernel_size=(
+            48, 48), activation='relu', input_shape=(self.IMAGE_WIDTH, self.IMAGE_HEIGHT, 1)))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(tf.keras.layers.Conv2D(
+            128, kernel_size=(24, 24), activation='relu'))
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(tf.keras.layers.Conv2DTranspose(128, (24, 24), strides=(
             2, 2), activation='relu', padding='same', kernel_initializer='uniform'))
-        self.model.add(layers.UpSampling2D(size=(2, 2)))
-        self.model.add(layers.Conv2DTranspose(64, (12, 12), strides=(
+        self.model.add(tf.keras.layers.UpSampling2D(size=(2, 2)))
+
+        self.model.add(tf.keras.layers.Conv2DTranspose(64, (12, 12), strides=(
             2, 2), activation='relu', padding='same', kernel_initializer='uniform'))
-        self.model.add(layers.UpSampling2D(size=(2, 2)))
+        self.model.add(tf.keras.layers.UpSampling2D(size=(2, 2)))
 
         # Cs Layers
-        self.model.add(layers.Conv2D(
+        self.model.add(tf.keras.layers.Conv2D(
             256, kernel_size=(12, 12), activation='relu'))
-        self.model.add(layers.Conv2D(
+        self.model.add(tf.keras.layers.Conv2D(
             256, kernel_size=(12, 12), activation='relu'))
-        self.model.add(layers.Conv2D(
+        self.model.add(tf.keras.layers.Conv2D(
             256, kernel_size=(12, 12), activation='relu'))
 
-        self.model.add(layers.Flatten())
-        self.model.add(layers.Dense(4096, activation='relu'))
-        self.model.add(layers.Dropout(0.5))
-        self.model.add(layers.Dense(4096, activation='relu'))
-        self.model.add(layers.Dropout(0.5))
-        self.model.add(layers.Dense(2383, activation='relu'))
-        self.model.add(layers.Dense(self.CLASS_COUNT, activation='softmax'))
+        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.Dense(512, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
+        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(
+            self.CLASS_COUNT, activation='softmax'))
 
-        sgd = keras.optimizers.SGD(
+        sgd = tf.keras.optimizers.SGD(
             lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
         self.model.compile(loss='mean_squared_error',
                            optimizer=sgd, metrics=['accuracy'])
@@ -99,14 +111,27 @@ class model:
     # trains a model on given data
 
     def train(self) -> None:
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath="Checkpoints",
+                                                         save_weights_only=True,
+                                                         verbose=1,
+                                                         period=1)
 
-        self.model.fit(self.train_ds, self.EPOCHS,
-                       validation_data=self.test_ds)
+        self.model.fit(self.train_ds, epochs=self.EPOCHS,
+                       validation_data=self.test_ds, callbacks=[cp_callback])
 
         return None
+
+    def save_model(self, dir: str = "MainModel.hdf5") -> None:
+        self.model.save(dir)
+
+    def load_model(self, dir: str = "MainModel.hdf5") -> None:
+        self.model = tf.keras.models.load_model(dir)
 
 
 m = model()
 m.load_dataset()
 m.build()
-# m.train()
+m.model.summary()
+m.train()
+m.save_model()
+m.load_model()
